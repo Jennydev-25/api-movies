934338d6d593b2dd4401208a5bbabbc1e98de079
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-29 23:59:04 +0200

    feat(globals): add global exception handler for genre not found

---
+++ src/main/java/dev/jenny/apimovies/globals/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package dev.jenny.apimovies.globals;
+
+import dev.jenny.apimovies.genre.exceptions.GenreException;
+import dev.jenny.apimovies.genre.exceptions.GenreExceptionNotFound;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(GenreExceptionNotFound.class)
+    public ResponseEntity<String> handleGenreNotFound(GenreExceptionNotFound exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(GenreException.class)
+    public ResponseEntity<String> handleGenreException(GenreException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor");
+    }
+}
