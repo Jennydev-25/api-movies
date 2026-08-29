600aeb5abbb9d4037826d2d8b6c0234033d10d7f
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-29 20:38:16 +0200

    feat(genre): add getters to GenreEntity

--- src/main/java/dev/jenny/apimovies/genre/GenreEntity.java
+++ src/main/java/dev/jenny/apimovies/genre/GenreEntity.java
@@ -22,4 +22,12 @@ public class GenreEntity {
         this.id = id;
         this.name = name;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
