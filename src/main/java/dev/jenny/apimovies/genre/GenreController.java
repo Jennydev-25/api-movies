196aa6c73ba449f8fdc281ed87313bf06d02551e
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 01:09:05 +0200

    feat(genre): add POST endpoint to create genres

--- src/main/java/dev/jenny/apimovies/genre/GenreController.java
+++ src/main/java/dev/jenny/apimovies/genre/GenreController.java
@@ -2,9 +2,16 @@ package dev.jenny.apimovies.genre;
 
 import java.util.List;
 
+import dev.jenny.apimovies.genre.dtos.GenreDTORequest;
 import dev.jenny.apimovies.genre.dtos.GenreDTOResponse;
+import dev.jenny.apimovies.implementations.InterfaceGenericEditService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenreController {
 
     private final InterfaceGenreService service;
+    private final InterfaceGenericEditService<GenreDTORequest, GenreDTOResponse> editService;
 
-    public GenreController(InterfaceGenreService service) {
+    public GenreController(InterfaceGenreService service,
+            InterfaceGenericEditService<GenreDTORequest, GenreDTOResponse> editService) {
         this.service = service;
+        this.editService = editService;
     }
 
     @GetMapping("")
@@ -26,5 +36,15 @@ public class GenreController {
     @GetMapping("{id}")
     public GenreDTOResponse getById(@PathVariable Long id) {
         return service.getById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<GenreDTOResponse> store(@Valid @RequestBody GenreDTORequest dto) {
+        GenreDTOResponse dtoResponse = editService.storeEntity(dto);
+
+        if (dtoResponse == null)
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+
+        return ResponseEntity.status(201).body(dtoResponse);
     }
 }
