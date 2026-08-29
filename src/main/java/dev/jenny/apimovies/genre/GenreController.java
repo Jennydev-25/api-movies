7e65eb7efeb20fc02de39cb88acd68a45a2d57e0
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-29 23:26:37 +0200

    feat(genre): add GenreController with index endpoint

---
+++ src/main/java/dev/jenny/apimovies/genre/GenreController.java
@@ -0,0 +1,24 @@
+package dev.jenny.apimovies.genre;
+
+import java.util.List;
+
+import dev.jenny.apimovies.genre.dtos.GenreDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}/genres")
+public class GenreController {
+
+    private final InterfaceGenreService service;
+
+    public GenreController(InterfaceGenreService service) {
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<GenreDTO> index() {
+        return service.getEntities();
+    }
+}
