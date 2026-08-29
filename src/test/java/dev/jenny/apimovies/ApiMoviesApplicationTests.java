3f36d74171ad39b28e63f2a822a9e89215dabb93
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-29 16:51:47 +0200

    build: add initial Spring Boot project structure and dependencies

---
+++ src/test/java/dev/jenny/apimovies/ApiMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jenny.apimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
