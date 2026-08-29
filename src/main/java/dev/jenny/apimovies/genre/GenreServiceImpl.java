357d21cec4f58af43cb5bbb467533592a388635f
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-29 21:28:42 +0200

    feat(genre): add GenreServiceImpl

---
+++ src/main/java/dev/jenny/apimovies/genre/GenreServiceImpl.java
@@ -0,0 +1,25 @@
+package dev.jenny.apimovies.genre;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenreServiceImpl implements InterfaceGenreService {
+
+    private final GenreRepository repository;
+
+    public GenreServiceImpl(GenreRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<GenreEntity> getEntities() {
+        return repository.findAll();
+    }
+
+    @Override
+    public GenreEntity getById(Long id) {
+        return null;
+    }
+}
