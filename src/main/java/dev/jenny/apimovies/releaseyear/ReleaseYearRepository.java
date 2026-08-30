57c92d46b8a069518db08849b1b0f3bb2256237c
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 05:07:04 +0200

    feat(release-year): add existsByReleaseYearAndIdNot query method to ReleaseYearRepository

--- src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearRepository.java
+++ src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearRepository.java
@@ -3,4 +3,5 @@ package dev.jenny.apimovies.releaseyear;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReleaseYearRepository extends JpaRepository<ReleaseYearEntity, Long> {
+    boolean existsByReleaseYearAndIdNot(Integer releaseYear, Long id);
 }
