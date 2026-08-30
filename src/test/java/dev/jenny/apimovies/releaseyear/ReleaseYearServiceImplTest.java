93e1d7648a0e1f7c2d181fbe94cd3909e18c45c7
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 04:10:15 +0200

    test(release-year): add test for ReleaseYearServiceImpl getEntities

---
+++ src/test/java/dev/jenny/apimovies/releaseyear/ReleaseYearServiceImplTest.java
@@ -0,0 +1,36 @@
+package dev.jenny.apimovies.releaseyear;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTOResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ReleaseYearServiceImplTest {
+
+    @InjectMocks
+    private ReleaseYearServiceImpl service;
+
+    @Mock
+    private ReleaseYearRepository repository;
+
+    @Test
+    void testGetEntities() {
+        List<ReleaseYearEntity> mock = List.of(new ReleaseYearEntity(1L, 1994), new ReleaseYearEntity(2L, 2001));
+        when(repository.findAll()).thenReturn(mock);
+
+        List<ReleaseYearDTOResponse> releaseYears = service.getEntities();
+
+        assertThat(releaseYears.size(), is(equalTo(2)));
+        assertThat(releaseYears.get(0).releaseYear(), is(equalTo(1994)));
+    }
+}
