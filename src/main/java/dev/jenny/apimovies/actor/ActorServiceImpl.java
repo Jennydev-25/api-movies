fa033653f86a2875bea8b3c6ba971518899da5d5
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 19:20:43 +0200

    feat(actor): implement storeEntity happy path in service

--- src/main/java/dev/jenny/apimovies/actor/ActorServiceImpl.java
+++ src/main/java/dev/jenny/apimovies/actor/ActorServiceImpl.java
@@ -2,14 +2,18 @@ package dev.jenny.apimovies.actor;
 
 import java.util.List;
 
+import dev.jenny.apimovies.actor.dtos.ActorDTORequest;
 import dev.jenny.apimovies.actor.dtos.ActorDTOResponse;
 import dev.jenny.apimovies.actor.exceptions.ActorExceptionNotFound;
 import dev.jenny.apimovies.actor.mappers.ActorMapper;
+import dev.jenny.apimovies.implementations.InterfaceGenericEditService;
 
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ActorServiceImpl implements InterfaceActorService {
+public class ActorServiceImpl implements InterfaceActorService,
+        InterfaceGenericEditService<ActorDTORequest, ActorDTOResponse> {
 
     private final ActorRepository repository;
 
@@ -30,5 +34,24 @@ public class ActorServiceImpl implements InterfaceActorService {
                 .orElseThrow(() -> new ActorExceptionNotFound(
                         "Actor not found. Id " + id + " does not exist."));
         return ActorMapper.toDTO(actor);
+    }
+
+    @Override
+    public ActorDTOResponse storeEntity(ActorDTORequest dto) {
+        ActorEntity actorToSave = ActorMapper.toEntity(dto);
+
+        Example<ActorEntity> example = Example.of(actorToSave);
+        boolean isEmpty = repository.findAll(example).isEmpty();
+
+        if (!isEmpty)
+            return null;
+
+        ActorEntity actorSaved = repository.save(actorToSave);
+        return ActorMapper.toDTO(actorSaved);
+    }
+
+    @Override
+    public ActorDTOResponse updateEntity(Long id, ActorDTORequest dto) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
