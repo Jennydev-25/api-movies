7319d3087f0383b415a9fae49ba82bbdfee74566
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 18:48:32 +0200

    test(actor): add test for getEntities

---
+++ src/test/java/dev/jenny/apimovies/actor/ActorServiceImplTest.java
@@ -0,0 +1,38 @@
+package dev.jenny.apimovies.actor;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import dev.jenny.apimovies.actor.dtos.ActorDTOResponse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ActorServiceImplTest {
+
+    @InjectMocks
+    private ActorServiceImpl service;
+
+    @Mock
+    private ActorRepository repository;
+
+    @Test
+    void testGetEntities() {
+        List<ActorEntity> mock = List.of(
+                new ActorEntity(1L, "Robert Downey Jr.", "American", LocalDate.of(1965, 4, 4)),
+                new ActorEntity(2L, "Scarlett Johansson", "American", LocalDate.of(1984, 11, 22)));
+        when(repository.findAll()).thenReturn(mock);
+        List<ActorDTOResponse> actors = service.getEntities();
+        assertThat(actors.size(), is(equalTo(2)));
+        assertThat(actors.get(0).name(), is(equalTo("Robert Downey Jr.")));
+    }
+}
