6d6c1d676bed34e489ccc3d9ed391e702c5d311e
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 03:55:57 +0200

    feat(release-year): implement ReleaseYearMapper toEntity

--- src/main/java/dev/jenny/apimovies/releaseyear/mappers/ReleaseYearMapper.java
+++ src/main/java/dev/jenny/apimovies/releaseyear/mappers/ReleaseYearMapper.java
@@ -1,6 +1,7 @@
 package dev.jenny.apimovies.releaseyear.mappers;
 
 import dev.jenny.apimovies.releaseyear.ReleaseYearEntity;
+import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTORequest;
 import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTOResponse;
 
 public class ReleaseYearMapper {
@@ -10,5 +11,11 @@ public class ReleaseYearMapper {
 
     public static ReleaseYearDTOResponse toDTO(ReleaseYearEntity entity) {
         return new ReleaseYearDTOResponse(entity.getId(), entity.getReleaseYear());
+    }
+
+    public static ReleaseYearEntity toEntity(ReleaseYearDTORequest dtoRequest) {
+        ReleaseYearEntity releaseYear = new ReleaseYearEntity();
+        releaseYear.setReleaseYear(dtoRequest.releaseYear());
+        return releaseYear;
     }
 }
