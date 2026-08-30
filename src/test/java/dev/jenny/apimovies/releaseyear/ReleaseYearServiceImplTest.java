260809280cb1a7e3749d14a4d82a918c05dd2ac1
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 04:26:55 +0200

    test(release-year): add test for ReleaseYearServiceImpl getById not found

--- src/test/java/dev/jenny/apimovies/releaseyear/ReleaseYearServiceImplTest.java
+++ src/test/java/dev/jenny/apimovies/releaseyear/ReleaseYearServiceImplTest.java
@@ -3,12 +3,15 @@ package dev.jenny.apimovies.releaseyear;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
 
 import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTOResponse;
+import dev.jenny.apimovies.releaseyear.exceptions.ReleaseYearExceptionNotFound;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -44,5 +47,15 @@ class ReleaseYearServiceImplTest {
 
         assertThat(releaseYear.id(), is(equalTo(1L)));
         assertThat(releaseYear.releaseYear(), is(equalTo(1994)));
+    }
+
+    @Test
+    void testGetById_NotFound_ShouldThrowException() {
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+
+        ReleaseYearExceptionNotFound exception = assertThrows(ReleaseYearExceptionNotFound.class,
+                () -> service.getById(1L));
+
+        assertThat(exception.getMessage(), is(equalTo("Release year not found. Id 1 does not exist.")));
     }
 }
