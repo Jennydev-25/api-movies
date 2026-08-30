56f5ec61df4b7d47e1d6c7cb0b275a95c593635a
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 17:35:23 +0200

    test(actor): add test for entity getters

--- src/test/java/dev/jenny/apimovies/actor/ActorEntityTest.java
+++ src/test/java/dev/jenny/apimovies/actor/ActorEntityTest.java
@@ -17,4 +17,13 @@ class ActorEntityTest {
         assertThat(actor, is(instanceOf(ActorEntity.class)));
         assertThat(actor.getClass().getDeclaredFields().length, is(equalTo(4)));
     }
+
+    @Test
+    void testActorEntity() {
+        ActorEntity actor = new ActorEntity(1L, "Robert Downey Jr.", "American", LocalDate.of(1965, 4, 4));
+        assertThat(actor.getId(), is(equalTo(1L)));
+        assertThat(actor.getName(), is(equalTo("Robert Downey Jr.")));
+        assertThat(actor.getNationality(), is(equalTo("American")));
+        assertThat(actor.getBirthDate(), is(equalTo(LocalDate.of(1965, 4, 4))));
+    }
 }
