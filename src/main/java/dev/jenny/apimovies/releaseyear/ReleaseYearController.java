78542f51884e5f0bfcb771e9de74189fec39db7d
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 12:21:17 +0200

    feat(release-year): add POST endpoint to create release years

--- src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearController.java
+++ src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearController.java
@@ -2,9 +2,18 @@ package dev.jenny.apimovies.releaseyear;
 
 import java.util.List;
 
+import dev.jenny.apimovies.implementations.InterfaceGenericEditService;
+import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTORequest;
 import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTOResponse;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReleaseYearController {
 
     private final InterfaceReleaseYearService service;
+    private final InterfaceGenericEditService<ReleaseYearDTORequest, ReleaseYearDTOResponse> editService;
 
-    public ReleaseYearController(InterfaceReleaseYearService service) {
+    public ReleaseYearController(InterfaceReleaseYearService service,
+            InterfaceGenericEditService<ReleaseYearDTORequest, ReleaseYearDTOResponse> editService) {
         this.service = service;
+        this.editService = editService;
     }
 
     @GetMapping("")
@@ -26,5 +38,13 @@ public class ReleaseYearController {
     @GetMapping("{id}")
     public ReleaseYearDTOResponse getById(@PathVariable Long id) {
         return service.getById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ReleaseYearDTOResponse> store(@Valid @RequestBody ReleaseYearDTORequest dto) {
+        ReleaseYearDTOResponse dtoResponse = editService.storeEntity(dto);
+        if (dtoResponse == null)
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        return ResponseEntity.status(201).body(dtoResponse);
     }
 }
