a4281b3e83f221f103758f0587e520b56fd5a313
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 18:27:58 +0200

    feat(actor): add mapper toEntity method

--- src/main/java/dev/jenny/apimovies/actor/mappers/ActorMapper.java
+++ src/main/java/dev/jenny/apimovies/actor/mappers/ActorMapper.java
@@ -1,6 +1,7 @@
 package dev.jenny.apimovies.actor.mappers;
 
 import dev.jenny.apimovies.actor.ActorEntity;
+import dev.jenny.apimovies.actor.dtos.ActorDTORequest;
 import dev.jenny.apimovies.actor.dtos.ActorDTOResponse;
 
 public class ActorMapper {
@@ -10,5 +11,13 @@ public class ActorMapper {
 
     public static ActorDTOResponse toDTO(ActorEntity entity) {
         return new ActorDTOResponse(entity.getId(), entity.getName(), entity.getNationality(), entity.getBirthDate());
+    }
+
+    public static ActorEntity toEntity(ActorDTORequest dtoRequest) {
+        ActorEntity actor = new ActorEntity();
+        actor.setName(dtoRequest.name());
+        actor.setNationality(dtoRequest.nationality());
+        actor.setBirthDate(dtoRequest.birthDate());
+        return actor;
     }
 }
