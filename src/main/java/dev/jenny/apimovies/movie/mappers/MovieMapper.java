ef875b9fe72056932be53d01d0b82d53d9243cde
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 23:38:25 +0200

    feat(movie): add mapper toDTO method

---
+++ src/main/java/dev/jenny/apimovies/movie/mappers/MovieMapper.java
@@ -0,0 +1,18 @@
+package dev.jenny.apimovies.movie.mappers;
+
+import dev.jenny.apimovies.movie.MovieEntity;
+import dev.jenny.apimovies.movie.dtos.MovieDTOResponse;
+
+import java.util.stream.Collectors;
+
+public class MovieMapper {
+
+    public static MovieDTOResponse toDTO(MovieEntity entity) {
+        return new MovieDTOResponse(
+                entity.getId(),
+                entity.getTitle(),
+                entity.getGenres().stream().map(genre -> genre.getName()).collect(Collectors.toSet()),
+                entity.getReleaseYear().getReleaseYear(),
+                entity.getActors().stream().map(actor -> actor.getName()).collect(Collectors.toSet()));
+    }
+}
