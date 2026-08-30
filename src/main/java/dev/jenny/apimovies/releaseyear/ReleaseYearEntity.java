e61f9d2d2613b375bc9228e6d32b6c9f6abc14c4
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 02:59:55 +0200

    feat(release-year): add ReleaseYearEntity with id and releaseYear

---
+++ src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearEntity.java
@@ -0,0 +1,25 @@
+package dev.jenny.apimovies.releaseyear;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "release_years")
+public class ReleaseYearEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer releaseYear;
+
+    public ReleaseYearEntity() {
+    }
+
+    public ReleaseYearEntity(Long id, Integer releaseYear) {
+        this.id = id;
+        this.releaseYear = releaseYear;
+    }
+}
