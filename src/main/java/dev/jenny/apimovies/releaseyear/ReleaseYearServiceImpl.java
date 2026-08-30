e91f14585d746268ed65e727df8d10743103aa9a
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 04:14:10 +0200

    feat(release-year): implement ReleaseYearServiceImpl getEntities

---
+++ src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearServiceImpl.java
@@ -0,0 +1,23 @@
+package dev.jenny.apimovies.releaseyear;
+
+import java.util.List;
+
+import dev.jenny.apimovies.releaseyear.dtos.ReleaseYearDTOResponse;
+import dev.jenny.apimovies.releaseyear.mappers.ReleaseYearMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReleaseYearServiceImpl implements InterfaceReleaseYearService {
+    private final ReleaseYearRepository repository;
+
+    public ReleaseYearServiceImpl(ReleaseYearRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<ReleaseYearDTOResponse> getEntities() {
+        return repository.findAll().stream()
+                .map(ReleaseYearMapper::toDTO)
+                .toList();
+    }
+}
