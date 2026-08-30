fdfc2292788d25f90ade03e399ba6b761ce46435
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 23:52:51 +0200

    feat(movie): add mapper toEntity method

--- src/main/java/dev/jenny/apimovies/movie/mappers/MovieMapper.java
+++ src/main/java/dev/jenny/apimovies/movie/mappers/MovieMapper.java
@@ -1,8 +1,13 @@
 package dev.jenny.apimovies.movie.mappers;
 
+import dev.jenny.apimovies.actor.ActorEntity;
+import dev.jenny.apimovies.genre.GenreEntity;
 import dev.jenny.apimovies.movie.MovieEntity;
+import dev.jenny.apimovies.movie.dtos.MovieDTORequest;
 import dev.jenny.apimovies.movie.dtos.MovieDTOResponse;
+import dev.jenny.apimovies.releaseyear.ReleaseYearEntity;
 
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MovieMapper {
@@ -17,5 +22,15 @@ public class MovieMapper {
                 entity.getGenres().stream().map(genre -> genre.getName()).collect(Collectors.toSet()),
                 entity.getReleaseYear().getReleaseYear(),
                 entity.getActors().stream().map(actor -> actor.getName()).collect(Collectors.toSet()));
+    }
+
+    public static MovieEntity toEntity(MovieDTORequest dto, Set<GenreEntity> genres, ReleaseYearEntity releaseYear,
+            Set<ActorEntity> actors) {
+        MovieEntity movie = new MovieEntity();
+        movie.setTitle(dto.title());
+        movie.setGenres(genres);
+        movie.setReleaseYear(releaseYear);
+        movie.setActors(actors);
+        return movie;
     }
 }
