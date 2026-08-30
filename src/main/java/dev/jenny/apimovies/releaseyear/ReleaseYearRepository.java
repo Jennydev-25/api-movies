16df5e3014ee6835698979b113f081209f082863
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-30 03:12:06 +0200

    feat(release-year): add ReleaseYearRepository

---
+++ src/main/java/dev/jenny/apimovies/releaseyear/ReleaseYearRepository.java
@@ -0,0 +1,6 @@
+package dev.jenny.apimovies.releaseyear;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReleaseYearRepository extends JpaRepository<ReleaseYearEntity, Long> {
+}
