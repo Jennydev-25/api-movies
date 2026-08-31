8740f6907125503bd3f935576ddadf858bab5b03
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-31 02:44:42 +0200

    feat(movie): add Movie repository findBy queries

--- src/main/java/dev/jenny/apimovies/movie/MovieRepository.java
+++ src/main/java/dev/jenny/apimovies/movie/MovieRepository.java
@@ -1,5 +1,7 @@
 package dev.jenny.apimovies.movie;
 
+import java.util.List;
+
 import dev.jenny.apimovies.releaseyear.ReleaseYearEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,8 @@ public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
     boolean existsByTitleAndReleaseYear(String title, ReleaseYearEntity releaseYear);
 
     boolean existsByTitleAndReleaseYearAndIdNot(String title, ReleaseYearEntity releaseYear, Long id);
+
+    List<MovieEntity> findByTitleContainingIgnoreCase(String title);
+
+    List<MovieEntity> findByGenres_NameContainingIgnoreCase(String genreName);
 }
